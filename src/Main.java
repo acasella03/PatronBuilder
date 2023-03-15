a397d6c88917490503dd4fc7189ab4941eb9b34c
Angi Marliu Casella <acasella@danielcastelao.org>
2023-03-15 14:28:12 +0100

    Creación de builderProyect

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        Pizzas pizzaAmedida;
+
+        //sintaxis NO reducida
+        BuilderPizzas miBuilder=new BuilderPizzas();
+        miBuilder.setRecogida(Pizzas.TIENDA);
+        miBuilder.setSinGluten(true);
+        pizzaAmedida=miBuilder.build();
+        System.out.println(pizzaAmedida.toString());
+
+        //sintaxis reducida pero poco legible
+        Pizzas pizzaDefault = new BuilderPizzas().setCebolla(false).setSize(Pizzas.BIG).build();
+        System.out.println(pizzaDefault.toString());
+
+        //sintaxis reducida legible
+        Pizzas pizzaDefault2=new BuilderPizzas().setCebolla(true).setSize(Pizzas.FINA).build();
+        System.out.println(pizzaDefault2.toString());
+
+        /*
+        // utilizamos el builder y en concreto el metodo build
+        // para tener nuestra pizza
+        Pizzas creadaConBuilder = new BuilderPizzas().build();
+        Pizzas sintaxisEncadenada = new BuilderPizzas()
+                .setCebolla(true)
+                .setSize(Pizzas.BIG)
+                .build();
+
+        // aunque el toSpring no está definida, nos valen estas lineas para poner
+        // un punto de ruptura y hacer debug
+        // asi podemos ver los objetos creados
+        System.out.println(creadaConBuilder.toString());
+        System.out.println(sintaxisEncadenada.toString());
+
+         */
+    }
+}
