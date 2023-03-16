892dba965205dd8c42ff97ebd5ee4e25e2f03e53
acasella03 <114586433+acasella03@users.noreply.github.com>
2023-03-16 18:32:23 +0100

    Creación de la rama builder y creación de pizzas en la main

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        Pizzas pizzaAmedida;
 
-        //sintaxis NO reducida
-        BuilderPizzas miBuilder = new BuilderPizzas();
-        miBuilder.setRecogida(TipoRecogida.PARA_LLEVAR);
-        miBuilder.setSinGluten(true);
-        miBuilder.setCebolla(false);
-        miBuilder.setRelleno(false);
-        miBuilder.setSize(Size.MEDIUM);
-        miBuilder.setTipoMasa(TipoMasa.NORMAL);
-        pizzaAmedida = miBuilder.build();
-        System.out.println(pizzaAmedida.toString());
+        Pizzas pizzaBasica=new BuilderPizzas().build();
+        System.out.println(pizzaBasica.toString());
 
-        //sintaxis reducida pero poco legible
-        Pizzas pizzaDefault = new BuilderPizzas().setSize(Size.BIG).setRelleno(true).build();
-        System.out.println(pizzaDefault.toString());
+        Pizzas pizzaBasicaExtraQueso=new BuilderPizzas().setTipoMasa(TipoMasa.NORMAL).setExtraQueso(true).build();
+        System.out.println(pizzaBasicaExtraQueso.toString());
 
-        //sintaxis reducida legible
-        Pizzas pizzaDefault2 = new BuilderPizzas().setCebolla(true).setSize(Size.BIG).setRecogida(TipoRecogida.PARA_LLEVAR).build();
-        System.out.println(pizzaDefault2.toString());
-
-        Pizzas pizzaAmedida2=new BuilderPizzas().setSize(Size.SMALL).setRelleno(false).setSalsa(false).setTipoMasa(TipoMasa.NORMAL).setExtraQueso(true).build();
-        System.out.println(pizzaAmedida2.toString());
-
-        Pizzas pizzaAmedida3=new BuilderPizzas().setSize(Size.BIG).setSalsa(true).setTipoSalsa(TipoSalsa.BARBACOA).setTipoMasa(TipoMasa.FINA).setRelleno(true).setCebolla(true).setExtraQueso(true).setMushrooms(true).setRecogida(TipoRecogida.PARA_LLEVAR).build();
-        System.out.println(pizzaAmedida3.toString());
+        Pizzas pizzaParaCeliacos=new BuilderPizzas().setSinGluten(true).setSize(Size.SMALL).setTipoSalsa(TipoSalsa.SALSA_SIN_GLUTEN).build();
+        System.out.println(pizzaParaCeliacos.toString());
     }
 }
